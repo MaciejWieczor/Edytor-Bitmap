a8d474e2e4aac5765e595e006aef7e9d3f23544f
Maciek <maciejwieczor@gmail.com>
2021-05-08 15:14:23 +0200

    Póki co ładuje obrazki i skaluje je bardzo ułomnie

--- gui.py
+++ gui.py
@@ -1,26 +1,60 @@
 import tkinter
+from tkinter import filedialog
 import PIL
 from PIL import ImageTk
+from PIL import Image
+import sys
+import os
 
 class GUI:
-    def __init__(self, obrazek_name):
+    def __init__(self):
         self.window = self.window_init()
-        self.obrazek = obrazek_name
-        self.image = self.image_param()
+        self.obrazek_name = self.browseFiles()
+        self.image = self.image()
         self.size = self.image.size
         self.width = self.size[0]+30
         self.height = self.size[1]+30
+        self.canvas = self.canvas_init()
         
     def window_init(self):
         return tkinter.Tk()
 
-    def image_param(self):
-        image = PIL.Image.open(self.obrazek)
+    def image(self):
+        image = PIL.Image.open(self.obrazek_name)
         return image
 
     def canvas_init(self):
-        canvas = tkinter.Canvas(self.window, width = self.width, height = self.height)
-        canvas.pack()
+        canvas = tkinter.Canvas(self.window, width = self.width, height = self.height, highlightthickness=1, highlightbackground="black")
         return canvas
 
+    @staticmethod
+    def browseFiles():
+        filename = filedialog.askopenfilename(initialdir = "C:/Users/macie/Source/Repos/Edytor_bitmap",
+                                              title = "Select a File",
+                                              filetypes = (("JPGs",
+                                                            "*.jpg*"),
+                                                           ("all files",
+                                                            "*.*")))
+      
+        # Change label contents
+        print(filename)
+        return filename
 
+    @staticmethod
+    def reset():
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
+
+    def save(self):
+        filename = filedialog.asksaveasfile(mode='w', defaultextension=".jpg")
+        if not filename:
+            return
+        self.image.save(filename)
+
+    def resize(self, mod):
+        self.image = self.image.resize((int(self.width*mod), int(self.height*mod)))
+        self.size = self.image.size
+        self.width = self.size[0]+30
+        self.height = self.size[1]+30
+    
+        
--- main_program.py
+++ main_program.py
@@ -1,18 +1,52 @@
 ﻿import tkinter as tk
 from PIL import ImageTk
+from PIL import Image
 from gui import GUI
+from numpy import asarray
+from functools import partial
+
 
 print("czeźdź")
 
-root = GUI("kek.jpg")
-canvas = root.canvas_init()
+global root
+root = GUI()
+
+root.canvas.pack()
 img = ImageTk.PhotoImage(root.image)
-canvas.create_image(20,20, anchor = tk.NW, image=img)
+image_id = root.canvas.create_image(20,20, anchor = tk.NW, image=img) 
+
+def resize():
+    global image_id
+    global img
+    root.canvas.delete(image_id)
+    root.resize(1.1)
+    #root.canvas = root.canvas_init()   #czy mogę zmienić wielkość canvas?
+    img = ImageTk.PhotoImage(root.image)
+    image_id = root.canvas.create_image(20,20, anchor = tk.NW, image=img) 
+
+def resize1():
+    global image_id
+    global img
+    root.canvas.delete(image_id)
+    root.resize(0.9)
+    img = ImageTk.PhotoImage(root.image)
+    image_id = root.canvas.create_image(20,20, anchor = tk.NW, image=img) 
+
+#przyciski
+
+exit_button = tk.Button(root.window, text="Exit", command=root.window.destroy)
+exit_button.pack(before = root.canvas)
+restart_button = tk.Button(root.window, text="Restart", command=root.reset)
+restart_button.pack(before = root.canvas)
+save_button = tk.Button(root.window, text="Save", command=root.save)
+save_button.pack(before = root.canvas)
+plus_size_button = tk.Button(root.window, text="+", command=resize)
+plus_size_button.pack(before = root.canvas)
+minus_size_button = tk.Button(root.window, text="-", command=resize1)
+minus_size_button.pack(before = root.canvas)
+
 root.window.mainloop()
 
-#root = tk.Tk()
-#canvas = tk.Canvas(root, width = 900, height = 1000)      
-#canvas.pack()      
-#img = ImageTk.PhotoImage(file="kek.jpg")      
-#canvas.create_image(20,20, anchor=tk.NW, image=img)      
-#tk.mainloop()
+
+
+
